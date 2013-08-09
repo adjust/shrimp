6fea2d5ba74cc2afcb83e9e68f93647ccfaf7c54
Justin Hnatow <justin.hnatow@parkassist.com>
2013-08-09 13:54:00 -0400

    Adds options for phantomjs and fail_silently. The phantomjs option defaults to the default phantom executable (which is defined in the     phantom model). The fail_silently option specifies whether exceptions should be explicitly raised or not.

--- lib/shrimp/configuration.rb
+++ lib/shrimp/configuration.rb
@@ -1,42 +1,41 @@
 require 'tmpdir'
+
+# Configure Phantomjs someplace sensible,
+# like config/initializers/phantomjs.rb
+#
+# @example
+#   Shrimp.configure do |config|
+#     config.phantomjs = '/usr/local/bin/phantomjs'
+#     config.format = 'Letter'
+#   end
 module Shrimp
   class Configuration
-    attr_accessor :default_options
-    attr_writer :phantomjs
+    attr_accessor :options
 
-    [:format, :margin, :zoom, :orientation, :tmpdir].each do |m|
+    [:format, :margin, :zoom, :orientation, :tmpdir, :phantomjs, 
+     :rendering_time, :fail_silently].each do |m|
       define_method("#{m}=") do |val|
-        @default_options[m]=val
+        @options[m]=val
       end
     end
 
     def initialize
-      @default_options = {
+      @options = {
           :format            => 'Letter',
           :margin            => '1cm',
           :zoom              => 1,
           :orientation       => 'portrait',
-          :tmpdir            => Dir.tmpdir
+          :tmpdir            => Dir.tmpdir,
+          :phantomjs         => Phantom.default_executable,
+          :rendering_time    => 30000,
+          :fail_silently     => false
       }
-    end
-
-    def phantomjs
-      @phantomjs ||= (defined?(Bundler::GemfileError) ? `bundle exec which phantomjs` : `which phantomjs`).chomp
     end
   end
 
   class << self
     attr_accessor :configuration
   end
-
-  # Configure Phantomjs someplace sensible,
-  # like config/initializers/phantomjs.rb
-  #
-  # @example
-  #   Shrimp.configure do |config|
-  #     config.phantomjs = '/usr/local/bin/phantomjs'
-  #     config.format = 'Letter'
-  #   end
 
   def self.configuration
     @configuration ||= Configuration.new
