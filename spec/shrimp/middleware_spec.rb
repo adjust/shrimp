b4779a3a12c8e84f716bfd5806f7f745aba60c4e
Tyler Rick <tyler.rick@k3integrations.com>
2013-11-20 09:30:18 -0800

    Minor cleanup in middleware_spec.rb

--- spec/shrimp/middleware_spec.rb
+++ spec/shrimp/middleware_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
-def app;
+def app
   Rack::Lint.new(@app)
 end
 
 def options
-  { :margin          => "1cm", :out_path => Dir.tmpdir,
-    :polling_offset  => 10, :polling_interval => 1, :cache_ttl => 3600,
-    :request_timeout => 1 }
+  { :margin         => "1cm",
+  :out_path         => Dir.tmpdir,
+  :polling_offset   => 10,
+  :polling_interval => 1,
+  :cache_ttl        => 3600,
+  :request_timeout  => 1 }
 end
 
 def mock_app(options = { }, conditions = { })
@@ -27,8 +30,9 @@ describe Shrimp::Middleware do
   context "matching pdf" do
     it "should render as pdf" do
       get '/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
+      @middleware.send(:render_as_pdf?).should be true
     end
+
     it "should return 503 the first time" do
       get '/test.pdf'
       last_response.status.should eq 503
@@ -42,7 +46,7 @@ describe Shrimp::Middleware do
       last_response.header["Retry-After"].should eq "1"
     end
 
-    it "should set render to to outpath" do
+    it "should set render_to to out_path" do
       get '/test.pdf'
       @middleware.send(:render_to).should match (Regexp.new("^#{options[:out_path]}"))
     end
@@ -64,42 +68,41 @@ describe Shrimp::Middleware do
 
     it "should return a pdf with 200 after rendering" do
       mock_file = double(File, :read => "Hello World", :close => true, :mtime => Time.now)
-      File.should_receive(:'exists?').and_return true
-      File.should_receive(:'size').and_return 1000
-      File.should_receive(:'open').and_return mock_file
-      File.should_receive(:'new').and_return mock_file
+      File.should_receive(:exists?).and_return true
+      File.should_receive(:size).and_return 1000
+      File.should_receive(:open).and_return mock_file
+      File.should_receive(:new).and_return mock_file
       get '/test.pdf'
       last_response.status.should eq 200
       last_response.body.should eq "Hello World"
     end
-
-
   end
+
   context "not matching pdf" do
     it "should skip pdf rendering" do
       get 'http://www.example.org/test'
       last_response.body.should include "Hello world!"
-      @middleware.send(:'render_as_pdf?').should be false
+      @middleware.send(:render_as_pdf?).should be false
     end
   end
 end
 
-describe "Conditions" do
+describe Shrimp::Middleware, "Conditions" do
   context "only" do
     before { mock_app(options, :only => [%r[^/invoice], %r[^/public]]) }
     it "render pdf for set only option" do
       get '/invoice/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
+      @middleware.send(:render_as_pdf?).should be true
     end
 
     it "render pdf for set only option" do
       get '/public/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
+      @middleware.send(:render_as_pdf?).should be true
     end
 
     it "not render pdf for any other path" do
       get '/secret/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be false
+      @middleware.send(:render_as_pdf?).should be false
     end
   end
 
@@ -107,17 +110,17 @@ describe "Conditions" do
     before { mock_app(options, :except => %w(/secret)) }
     it "render pdf for set only option" do
       get '/invoice/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
+      @middleware.send(:render_as_pdf?).should be true
     end
 
     it "render pdf for set only option" do
       get '/public/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
+      @middleware.send(:render_as_pdf?).should be true
     end
 
     it "not render pdf for any other path" do
       get '/secret/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be false
+      @middleware.send(:render_as_pdf?).should be false
     end
   end
 end
