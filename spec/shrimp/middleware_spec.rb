5cde07a6a186257acb0de3d25a5420e248e3e861
Martin Gotink <m.gotink@gmail.com>
2014-03-21 12:38:01 +0100

    Refactor middleware spec

--- spec/shrimp/middleware_spec.rb
+++ spec/shrimp/middleware_spec.rb
@@ -24,11 +24,7 @@ end
 describe Shrimp::Middleware do
   before { mock_app(options) }
 
-  context "matching pdf" do
-    it "should render as pdf" do
-      get '/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
-    end
+  describe "rendering the PDF" do
     it "should return 503 the first time" do
       get '/test.pdf'
       last_response.status.should eq 503
@@ -40,11 +36,6 @@ describe Shrimp::Middleware do
       get '/test.pdf'
       last_response.status.should eq 503
       last_response.header["Retry-After"].should eq "1"
-    end
-
-    it "should set render to to outpath" do
-      get '/test.pdf'
-      @middleware.send(:render_to).should match (Regexp.new("^#{options[:out_path]}"))
     end
 
     it "should return 504 on timeout" do
@@ -72,52 +63,77 @@ describe Shrimp::Middleware do
       last_response.status.should eq 200
       last_response.body.should eq "Hello World"
     end
-
-
   end
-  context "not matching pdf" do
-    it "should skip pdf rendering" do
-      get 'http://www.example.org/test'
-      last_response.body.should include "Hello world!"
-      @middleware.send(:'render_as_pdf?').should be false
-    end
-  end
-end
 
-describe "Conditions" do
-  context "only" do
-    before { mock_app(options, :only => [%r[^/invoice], %r[^/public]]) }
-    it "render pdf for set only option" do
-      get '/invoice/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
-    end
-
-    it "render pdf for set only option" do
-      get '/public/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
-    end
-
-    it "not render pdf for any other path" do
-      get '/secret/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be false
+  describe "#render_to" do
+    it "should be set to the outpath" do
+      get '/test.pdf'
+      @middleware.send(:render_to).should match (Regexp.new("^#{options[:out_path]}"))
     end
   end
 
-  context "except" do
-    before { mock_app(options, :except => %w(/secret)) }
-    it "render pdf for set only option" do
-      get '/invoice/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
+  describe "#render_as_pdf?" do
+    context "without conditions" do
+      context "the URL contains .pdf" do
+        it "should render as pdf" do
+          get '/test.pdf'
+          @middleware.send(:'render_as_pdf?').should be true
+        end
+      end
+
+      context "the URL doesn't contain .pdf" do
+        it "should skip pdf rendering" do
+          get 'http://www.example.org/test'
+          last_response.body.should include "Hello world!"
+          @middleware.send(:'render_as_pdf?').should be false
+        end
+      end
     end
 
-    it "render pdf for set only option" do
-      get '/public/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be true
+    context "with only condition" do
+      before { mock_app(options, :only => [%r[^/invoice], %r[^/public]]) }
+
+      context "when the url is in the only condition" do
+        it "render pdf for set only option" do
+          get '/invoice/test.pdf'
+          @middleware.send(:'render_as_pdf?').should be true
+        end
+
+        it "render pdf for set only option" do
+          get '/public/test.pdf'
+          @middleware.send(:'render_as_pdf?').should be true
+        end
+      end
+
+      context "when the url isn't in the only condition" do
+        it "not render pdf for any other path" do
+          get '/secret/test.pdf'
+          @middleware.send(:'render_as_pdf?').should be false
+        end
+      end
     end
 
-    it "not render pdf for any other path" do
-      get '/secret/test.pdf'
-      @middleware.send(:'render_as_pdf?').should be false
+    context "with except condition" do
+      before { mock_app(options, :except => %w(/secret)) }
+
+      context "when the url isn't in the except condition" do
+        it "render pdf for set only option" do
+          get '/invoice/test.pdf'
+          @middleware.send(:'render_as_pdf?').should be true
+        end
+
+        it "render pdf for set only option" do
+          get '/public/test.pdf'
+          @middleware.send(:'render_as_pdf?').should be true
+        end
+      end
+
+      context "when the url is in the except condition" do
+        it "not render pdf for any other path" do
+          get '/secret/test.pdf'
+          @middleware.send(:'render_as_pdf?').should be false
+        end
+      end
     end
   end
 end
